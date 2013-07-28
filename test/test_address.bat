cd53d29fe997441a1d8324deb0f40c48afdc54f6
MITSUNARI Shigeo <herumi@nifty.com>
2013-07-28 23:04:49 +0900

    concat binary for long seq

--- test/test_address.bat
+++ test/test_address.bat
@@ -25,7 +25,7 @@ cl address.cpp %OPT% %OPT2%
 address %1% > a.asm
 echo nasm -f %OPT3% -l a.lst a.asm
 nasm -f %OPT3% -l a.lst a.asm
-awk "!/warning:/ {print $3}" < a.lst > ok.lst
+awk "{if (index($3, ""-"")) { conti=substr($3, 0, length($3) - 1) } else { conti = conti $3; print conti; conti = """" }} " < a.lst |%FILTER% > ok.lst
 echo address %1% jit > nm.cpp
 address %1% jit > nm.cpp
 echo cl -I../ -DXBYAK_TEST nm_frame.cpp %OPT% %OPT2%
