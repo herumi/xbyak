cc7fa5086f5a2b2bdfc69417a8484982a70bebbe
MITSUNARI Shigeo <herumi@nifty.com>
2016-11-27 09:30:15 +0900

    update version

--- xbyak/xbyak.h
+++ xbyak/xbyak.h
@@ -105,7 +105,7 @@ namespace Xbyak {
 
 enum {
 	DEFAULT_MAX_CODE_SIZE = 4096,
-	VERSION = 0x5200 /* 0xABCD = A.BC(D) */
+	VERSION = 0x5300 /* 0xABCD = A.BC(D) */
 };
 
 #ifndef MIE_INTEGER_TYPE_DEFINED
