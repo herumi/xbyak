2c54c052e4cc1ae5fb2c7c73d79228b0f40ab554
MITSUNARI Shigeo <herumi@nifty.com>
2016-12-08 21:27:01 +0900

    use stable sort

--- gen/update.bat
+++ gen/update.bat
@@ -1,16 +1,17 @@
 @echo off
 set OPT=/EHsc -I../xbyak /W4 -D_CRT_SECURE_NO_WARNINGS
 set TARGET=..\\xbyak\\xbyak_mnemonic.h
+set SORT=sortline
 cl gen_code.cpp %OPT%
-gen_code | sort > %TARGET%
+gen_code | %SORT% > %TARGET%
 echo #ifdef XBYAK_ENABLE_OMITTED_OPERAND>> %TARGET%
-gen_code omit | sort >> %TARGET%
+gen_code omit | %SORT% >> %TARGET%
 echo #endif>>%TARGET%
 gen_code fixed >> %TARGET%
 cl gen_avx512.cpp %OPT%
 echo #ifndef XBYAK_DISABLE_AVX512>> %TARGET%
-gen_avx512 |sort >> %TARGET%
+gen_avx512 |%SORT% >> %TARGET%
 echo #ifdef XBYAK64>> %TARGET%
-gen_avx512 64 |sort >> %TARGET%
+gen_avx512 64 |%SORT% >> %TARGET%
 echo #endif>> %TARGET%
 echo #endif>> %TARGET%
