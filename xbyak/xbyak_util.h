f6678587615bfca1c8aa324dc1d2c7b4d0001a4b
MITSUNARI Shigeo <herumi@nifty.com>
2022-12-07 16:09:18 +0900

    add detection of prefetchiti

--- xbyak/xbyak_util.h
+++ xbyak/xbyak_util.h
@@ -416,6 +416,7 @@ public:
 	XBYAK_DEFINE_TYPE(71, tAVX_IFMA);
 	XBYAK_DEFINE_TYPE(72, tRAO_INT);
 	XBYAK_DEFINE_TYPE(73, tCMPCCXADD);
+	XBYAK_DEFINE_TYPE(74, tPREFETCHITI);
 
 #undef XBYAK_SPLIT_ID
 #undef XBYAK_DEFINE_TYPE
@@ -565,6 +566,7 @@ public:
 				if (EAX & (1U << 23)) type_ |= tAVX_IFMA;
 				if (EDX & (1U << 4)) type_ |= tAVX_VNNI_INT8;
 				if (EDX & (1U << 5)) type_ |= tAVX_NE_CONVERT;
+				if (EDX & (1U << 14)) type_ |= tPREFETCHITI;
 			}
 		}
 		setFamily();
