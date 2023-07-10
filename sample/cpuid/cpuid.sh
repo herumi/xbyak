5921e270c22d51172461b3ea4e8029c14c94c6d6
MITSUNARI Shigeo <herumi@nifty.com>
2023-07-10 12:40:14 +0900

    update cpuid

--- sample/cpuid/cpuid.sh
+++ sample/cpuid/cpuid.sh
@@ -2,7 +2,8 @@
 
 make -C ../ test_util64
 
-cpus=(p4p mrm pnr nhm wsm snb ivb hsw bdw slt slm glm glp tnt skl cnl icl skx clx cpx icx knl knm tgl adl spr)
+cpus=(p4p mrm pnr nhm wsm snb ivb hsw bdw slt slm glm glp tnt skl cnl icl skx clx cpx icx knl knm tgl adl mtl rpl spr gnr srf grr)
+
 for cpu in ${cpus[@]} ; do
   echo $cpu
   ~/bin/sde -$cpu -- ../test_util64 -cpuid > tmp.txt
