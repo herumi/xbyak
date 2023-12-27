5c95842bebbd005e0fbfe48692ae066361b18a7b
MITSUNARI Shigeo <herumi@nifty.com>
2023-12-27 09:16:06 +0900

    tweak

--- sample/no_flags.cpp
+++ sample/no_flags.cpp
@@ -3,16 +3,13 @@
 
 struct Code : Xbyak::CodeGenerator {
 	Code(bool nf) {
-		if (nf) {
-			puts("no flags (with T_nf)");
-		} else {
-			puts("change flags (without T_nf)");
-		}
 		xor_(eax, eax); // CF = 0
 		mov(eax, -1);
 		if (nf) {
+			puts("no flags (with T_nf)");
 			add(eax|T_nf, eax, 1); // does not change CF
 		} else {
+			puts("change flags (without T_nf)");
 			add(eax, eax, 1); // CF = 1
 		}
 		adc(eax, 0); // eax = CF ? 1 : 0
