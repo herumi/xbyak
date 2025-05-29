55781701f61d8bacbc61b3c30fa0eaa6b710f094
MITSUNARI Shigeo <herumi@nifty.com>
2025-05-30 06:25:18 +0900

    avx_type_def.h is updated

--- gen/avx_type_def.h
+++ gen/avx_type_def.h
@@ -50,6 +50,7 @@
 	static const uint64_t T_SENTRY = (1ull << 38)-1; // attribute(>=T_SENTRY) is for error check
 	static const uint64_t T_ALLOW_DIFF_SIZE = 1ull << 38; // allow difference reg size
 	static const uint64_t T_ALLOW_ABCDH = 1ull << 39; // allow [abcd]h reg
+	static const uint64_t T_FP8 = 1ull << 40; // amx bf8 and hf8
 	// T_66 = 1, T_F3 = 2, T_F2 = 3
 	static inline uint32_t getPP(uint64_t type) { return (type & T_66) ? 1 : (type & T_F3) ? 2 : (type & T_F2) ? 3 : 0; }
 	// @@@end of avx_type_def.h
