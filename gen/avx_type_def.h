bf622207eda133891cbb51fa89e38a0b005a4fef
MITSUNARI Shigeo <herumi@nifty.com>
2025-06-01 05:15:08 +0900

    merge T_EW0 into T_W0

--- gen/avx_type_def.h
+++ gen/avx_type_def.h
@@ -21,7 +21,7 @@
 	static const uint64_t T_L1 = 1ull << 12;
 	static const uint64_t T_W0 = 1ull << 13;
 	static const uint64_t T_W1 = 1ull << 14;
-	static const uint64_t T_EW0 = 1ull << 15;
+	static const uint64_t T_EW0 = T_W0;//1ull << 15;
 	static const uint64_t T_EW1 = 1ull << 16;
 	static const uint64_t T_YMM = 1ull << 17; // support YMM, ZMM
 	static const uint64_t T_EVEX = 1ull << 18;
