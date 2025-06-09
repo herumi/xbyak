ceed48edea8e466f1a7027fdfd5bd6b611826eee
MITSUNARI Shigeo <herumi@nifty.com>
2025-06-10 06:56:41 +0900

    rename test/avx10 to test/dataset

--- test/test_by_xed_all.bat
+++ test/test_by_xed_all.bat
@@ -1,5 +1,5 @@
 set TARGETS=old.txt new-ymm.txt bf16.txt misc.txt convert.txt minmax.txt saturation.txt amx.txt apx.txt comp.txt
 for %%f in (%TARGETS%) do (
   echo %%f
-  call test_by_xed.bat avx10\%%f
+  call test_by_xed.bat dataset\%%f
 )
