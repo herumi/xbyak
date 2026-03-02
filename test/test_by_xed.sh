093fa48eec0912e1e033654e192af4795a8dae2d
MITSUNARI Shigeo <herumi@nifty.com>
2026-03-03 08:18:32 +0900

    fix xed_test script

--- test/test_by_xed.sh
+++ test/test_by_xed.sh
@@ -2,8 +2,8 @@
 
 set -e
 XED=${XED:=xed}
-#XED_OPT="-64 -set PREFETCHIT 1 -set PREFETCHRST 1"
-XED_OPT=${XED_OPT:=-64 -chip-set FUTURE}
+XED_OPT=${XED_OPT:=-64 -set PREFETCHIT 1 -set PREFETCHRST 1}
+#XED_OPT=${XED_OPT:=-64 -chip-check FUTURE}
 CXX=${CXX:=g++}
 CFLAGS_USER=${CFLAGS}
 CFLAGS_WARN="$(cat CFLAGS_WARN.cfg)"
@@ -22,7 +22,8 @@ CFLAGS="$CFLAGS_USER $CFLAGS_WARN -I ../"
 echo "test:" $TARGET
 cp $TARGET tmp.cpp
 $CXX $CFLAGS test_by_xed.cpp -o test_by_xed
-./test_by_xed
-$XED ${XED_OPT} -ir bin > out.txt
-$PYTHON test_by_xed.py $TARGET out.txt
+./test_by_xed || (echo "ERR test_by_xed"; exit 1)
+echo "$XED ${XED_OPT} -ir bin > out.txt"
+$XED ${XED_OPT} -ir bin > out.txt || (echo "ERR xed"; exit 1)
+$PYTHON test_by_xed.py $TARGET out.txt || (echo "ERR test_by_xed.py"; exit 1)
 
