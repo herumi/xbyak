037548f901f798350f9e3727589acbfff392f7f4
MITSUNARI Shigeo <herumi@nifty.com>
2026-05-14 08:14:29 +0900

    v7.37.1

--- xbyak/xbyak.h
+++ xbyak/xbyak.h
@@ -177,7 +177,7 @@ namespace Xbyak {
 
 enum {
 	DEFAULT_MAX_CODE_SIZE = 4096,
-	VERSION = 0x7370 /* 0xABCD = A.BC(.D) */
+	VERSION = 0x7371 /* 0xABCD = A.BC(.D) */
 };
 
 #ifndef MIE_INTEGER_TYPE_DEFINED
