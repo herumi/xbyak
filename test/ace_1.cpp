27441cc252080cf0cd276ff166b04764f948e74f
MITSUNARI Shigeo <herumi@nifty.com>
2026-08-14 18:00:17 +0900

    add negative tests for operand guards of ace_1

--- test/ace_1.cpp
+++ test/ace_1.cpp
@@ -292,6 +292,31 @@ CYBOZU_TEST_AUTO(ace_1_unpackb)
 	CYBOZU_TEST_EQUAL_ARRAY(c.getCode(), tbl, n);
 }
 
+// negative tests for the operand guards (opCvt1/opCvt7/opVmov)
+CYBOZU_TEST_AUTO(ace_1_bad_combination)
+{
+	struct Code : Xbyak::CodeGenerator {
+		Code()
+		{
+			// vcvt{bf,hf}82ps : src is xmm or mem regardless of dst width
+			CYBOZU_TEST_EXCEPTION(vcvtbf82ps(zm1, ym2), std::exception);
+			CYBOZU_TEST_EXCEPTION(vcvthf82ps(xm1, zm2), std::exception);
+			// vcvtbf42hf8 : (x|y, x/m), (z, y/m)
+			CYBOZU_TEST_EXCEPTION(vcvtbf42hf8(zm1, xm2), std::exception);
+			CYBOZU_TEST_EXCEPTION(vcvtbf42hf8(xm1, ym2), std::exception);
+			// vcvtbiasps2* : dst is fixed xmm and bias/src widths must match
+			CYBOZU_TEST_EXCEPTION(vcvtbiasps2bf8(ym1, zm2, zm3), std::exception);
+			CYBOZU_TEST_EXCEPTION(vcvtbiasps2bf8(xm1, ym2, zm3), std::exception);
+			CYBOZU_TEST_EXCEPTION(vcvtbiasps2hf8s(xm1, zm2, ym3), std::exception);
+			// vcvt{bf,hf}82bf4s : (x, x), (x, y), (y, z) only
+			CYBOZU_TEST_EXCEPTION(vcvtbf82bf4s(ym1, ym2), std::exception);
+			CYBOZU_TEST_EXCEPTION(vcvthf82bf4s(xm1, zm2), std::exception);
+			// vpmovssdb : dst is xmm or mem
+			CYBOZU_TEST_EXCEPTION(vpmovssdb(ym1, ym2), std::exception);
+		}
+	} c;
+}
+
 CYBOZU_TEST_AUTO(ace_1_pmovssdb)
 {
 	struct Code : Xbyak::CodeGenerator {
